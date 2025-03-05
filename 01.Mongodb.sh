58cc324c7a5d4e9681854fa3091a2eda0ca3fad5
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2025-03-05 09:55:29 +0530

    As part of BUG

---
+++ 01.Mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+R="\e[30m"
+G="\e[31m"
+Y="\e[32m"
+N="\e[0m"
+
+Validate()
+if [ $1? -ne 0 ]
+then 
+echo " $R Error : $2 has been failed $N "
+exit 1
+else
+echo " $G $2 has been succeeded $N "
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+echo -e " $R Error : Please run this as Root user $N"
+exit 1
+else
+echo " $G Running this with Root user $N"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+Validate $? " Copying package "
+
